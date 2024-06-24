d94d78797939ec612721b083332f8d79e97bdfde
Kunalsahuji <ksahu0103@gmail.com>
2024-06-24 12:53:29 +0530

    db online connected

--- models/dbConnection.js
+++ models/dbConnection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://0.0.0.0/OneToOneChat").then(() => {
+mongoose.connect("mongodb+srv://whatsapp:kunal@kunalcluster.beakm07.mongodb.net/oneToOneChat?retryWrites=true&w=majority&appName=kunalcluster").then(() => {
     console.log("DB connected!")
 }).catch((error) => {
     console.log(error.message)
