f48b450b6097f4ec9b64084a9199123dfa4404cb
Kunalsahuji <ksahu0103@gmail.com>
2024-08-10 00:18:30 +0530

    UI ✅

--- socketapi.js
+++ socketapi.js
@@ -3,50 +3,59 @@ const socketapi = {
     io: io
 };
 
-const user = require('./models/userModel')
-const Message = require('./models/messageModel')
+const User = require('./models/userModel');
+const Message = require('./models/messageModel');
+
 // Add your socket.io logic here!
-io.on("connection", function (socket) {
+io.on("connection", (socket) => {
     console.log("A user connected");
+
+    // Join event: Update user's socket ID
     socket.on("join", async (username) => {
-        await user.findOneAndUpdate({ username }, { socketId: socket.id })
-    })
-
-    // disconnect
-    socket.on('disconnect', async () => {
-        await user.findOneAndUpdate(
-            {
-                socketId: socket.id
-            },
-            { socketId: "" }
-        )
-    })
-
-    // sender receiver text
-    // sony
-    socket.on("sony", async (messageObject) => {
-        await Message.create({
-            receiver: messageObject.receiver,
-            sender: messageObject.sender,
-            text: messageObject.text
-        })
-        const sender = await user.findOne({
-            username: messageObject.sender
-        })
-        const receiver = await user.findOne({
-            username: messageObject.receiver
-        })
-        const messagePacket = {
-            sender: sender,
-            receiver: receiver,
-            text: messageObject.text
+        try {
+            await User.findOneAndUpdate({ username }, { socketId: socket.id });
+        } catch (error) {
+            console.error("Error updating user socket ID:", error);
         }
+    });
 
-        // max
-        socket.to(receiver.socketId).emit("max", messagePacket)
-    })
+    // Disconnect event: Clear user's socket ID
+    socket.on('disconnect', async () => {
+        try {
+            await User.findOneAndUpdate({ socketId: socket.id }, { socketId: "" });
+        } catch (error) {
+            console.error("Error clearing user socket ID:", error);
+        }
+    });
+
+    // Handle incoming messages
+    socket.on("sony", async (messageObject) => {
+        try {
+            // Save the message in the database
+            const newMessage = await Message.create({
+                receiver: messageObject.receiver,
+                sender: messageObject.sender,
+                text: messageObject.text
+            });
+
+            // Find sender and receiver user objects
+            const sender = await User.findOne({ username: messageObject.sender });
+            const receiver = await User.findOne({ username: messageObject.receiver });
+
+            // Prepare message packet
+            const messagePacket = {
+                sender: sender,
+                receiver: receiver,
+                text: messageObject.text
+            };
+
+            // Emit message to the receiver
+            socket.to(receiver.socketId).emit("max", messagePacket);
+        } catch (error) {
+            console.error("Error handling message:", error);
+        }
+    });
 });
 
-// end of socket.io logic
-
+// End of socket.io logic
 module.exports = socketapi;
